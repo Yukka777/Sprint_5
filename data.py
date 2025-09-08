638944931c0d73eb0b2e2f579df69f8a5bde83f5
Yukka777 <109177551+Yukka777@users.noreply.github.com>
2025-09-08 13:59:03 +0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,10 @@
+class URLS:
+    MAIN = 'https://stellarburgers.nomoreparties.site'  # главная страница
+    REG = 'https://stellarburgers.nomoreparties.site/register'  # регистрация
+    LOGIN = 'https://stellarburgers.nomoreparties.site/login'  # авторизация
+    FORGOT_PASS = 'https://stellarburgers.nomoreparties.site/forgot-password'  # восстановление пароля
+
+class Auth:
+    name = 'Julia'
+    email = 'jgarmay@gmail.ru'
+    password = '123456789'
