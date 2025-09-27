8917fc6525e9c89ed2e397089689c3460087df20
Yukka777 <109177551+Yukka777@users.noreply.github.com>
2025-09-27 18:43:52 +0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,8 @@
+
+class UserData:
+    correct_email = "juliagarmay@gmail.com"
+    password = "qwerty"
+    not_correct_email = "invalid-email"
+    name_product = "Монополия"
+    description = "Настольная игра"
+    price = 2750
